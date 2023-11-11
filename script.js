9d45ad903f739044b3b91a1bebd31911cc3b696a
Sajal Kapoor <kapoorsajal91@gmail.com>
2023-11-11 17:26:21 +0530

    again changes

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let getstartedBtn = document.querySelector('#getstarted-btn');
 let icon = document.querySelector('#lessthanicon');
+let emailInput = document.querySelector('#email-input');
+let body = document.querySelector('body');
+
+console.dir(emailInput);
 
 let styling = function () {
     icon.style.backgroundColor = "rgb(255, 0, 0, 0.1)";
     icon.style.transitionDuration = "0.3s";
 }
 
-getstartedBtn.addEventListener('mouseenter',styling());
+let styling2 = function () {
+    emailInput.setAttribute('placeholder','');
+}
+
+getstartedBtn.addEventListener('mouseenter',styling);
+emailInput.addEventListener('click',styling2);
