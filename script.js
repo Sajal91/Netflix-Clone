c0829cc34e299ca6aea40c7b0262baa72f6270cb
Sajal Kapoor <kapoorsajal91@gmail.com>
2023-11-14 13:20:14 +0530

    js added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let getstartedBtn = document.querySelector('#getstarted-btn');
 let icon = document.querySelector('#lessthanicon');
 let emailInput = document.querySelector('#email-input');
+let emailPlaceholder = document.querySelector('#email-placeholder');
 let body = document.querySelector('body');
 
 console.dir(emailInput);
@@ -12,7 +13,10 @@ let styling = function () {
 
 let styling2 = function () {
     emailInput.setAttribute('placeholder','');
+    emailPlaceholder.setAttribute('placeholder','Email Address');
+    emailPlaceholder.style.display = "block";
+    emailPlaceholder.disabled = true;
 }
 
 getstartedBtn.addEventListener('mouseenter',styling);
-emailInput.addEventListener('click',styling2);
+emailInput.addEventListener('mousedown',styling2);
